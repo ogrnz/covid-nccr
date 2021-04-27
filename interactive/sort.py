c02b759deb37ea49a854662c83e681fd19f07f7c
Olivier Grognuz <olivier.grognuz@gmail.com>
2021-04-27 18:49:07 +0200

    date sort bug

--- interactive/sort.py
+++ interactive/sort.py
@@ -26,31 +26,41 @@ with db:
 len(tweets)
 
 # %%
-df = Helpers.df_from_db(tweets)
+df_all = Helpers.df_from_db(tweets)
 
+#%%
 # and sort it
-df = df[(df["covid_theme"] == 1) & ~(df["theme_hardcoded"] == "0")]
-df = df[
-    ~(df["topic"].isin(Helpers.topics_cov))
-    & ~(df["topic"].isin(Helpers.topics_not_cov))
-]
+df_yes = df_all[(df_all["covid_theme"] == 1) & ~(df_all["theme_hardcoded"] == "0")]
+df = df_yes[
+    ~(df_yes["topic"].isin(Helpers.topics_cov))
+    & ~(df_yes["topic"].isin(Helpers.topics_not_cov))
+].copy()
 
 #%%
 def convert_date(datestr):
-    return datestr.split(" ")[0]
+    datestr = datestr.split(" ")[0]
+    return datestr.replace("-", "/")
 
 
 #%%
 # Convert date to be handled
-df["created_at"] = df["created_at"].apply(convert_date)
-df["created_at"] = pd.to_datetime(df["created_at"])
+df["date"] = df["created_at"].apply(convert_date)
+df["date"] = pd.to_datetime(df["date"], format="%d/%m/%Y")
 
 
 # %%
 # only keep between 2020/09/01 and 2021/03/31
-df = df[
-    (df["created_at"] >= pd.Timestamp(2020, 9, 1))
-    & (df["created_at"] <= pd.Timestamp(2021, 3, 31))
+start = "31/08/2020"
+end = "01/04/2021"
+
+df_sorted = df[
+    (df["date"] > pd.to_datetime(start, format="%d/%m/%Y"))
+    & (df["date"] < pd.to_datetime(end, format="%d/%m/%Y"))
 ]
-df
+df_sorted
+# 28931 tweets
+
+# %%
+df_sorted.to_excel("database/xlsx/Full_2nd_wave.xlsx", index=False)
+
 # %%
